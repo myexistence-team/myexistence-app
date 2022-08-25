4fb93dbc78c19a0c9af44aa41ebd60ebd2f68c59
FarizioKautsar <kheruzy@gmail.com>
2022-08-25 20:18:15 +0700

    updated google client ids

--- googleClientIds.ts
+++ googleClientIds.ts
@@ -1,6 +1,6 @@
 export default {
   iosClientId: '279276711391-m70ho03t1hd9ju0hk5feqreau2ohi453.apps.googleusercontent.com',
-  androidClientId: '279276711391-o67jo1rh7n5pg8vfq37eg4pv615td69k.apps.googleusercontent.com',
-  expoClientId: '279276711391-8uemdj81dtk9iavgf7jjflvrdnv5e0mq.apps.googleusercontent.com',
+  androidClientId: '279276711391-1pfl57ibsqabnlbsiq75fvifh16rnulf.apps.googleusercontent.com',
+  expoClientId: '279276711391-o67jo1rh7n5pg8vfq37eg4pv615td69k.apps.googleusercontent.com',
   expoClientSecret: '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'
 }
