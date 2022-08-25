19eee86ab50a71b1bb7bd23ccd63e4fe3cac8832
FarizioKautsar <kheruzy@gmail.com>
2022-08-26 01:32:27 +0700

    Added constants

---
+++ constants/constants.ts
@@ -0,0 +1,27 @@
+export const SCHEDULE_START_DATE_MS = 259200000;
+
+const nowSchedule = new Date();
+nowSchedule.setDate(4);
+nowSchedule.setFullYear(1970);
+nowSchedule.setMonth(0);
+
+export { nowSchedule };
+
+export const DAY_NUMBERS = {
+  0: "Minggu",
+  1: "Senin",
+  2: "Selasa",
+  3: "Rabu",
+  4: "Kamis",
+  5: "Jumat",
+  6: "Sabtu",
+}
+export const DAYS_ARRAY = [
+  "Minggu",
+  "Senin",
+  "Selasa",
+  "Rabu",
+  "Kamis",
+  "Jumat",
+  "Sabtu",
+]
