ecf2c8af02e2b2b677bf469536605ace6875e464
FarizioKautsar <kheruzy@gmail.com>
2022-08-25 13:22:45 +0700

    MESelect styling

--- components/MESelect.tsx
+++ components/MESelect.tsx
@@ -8,8 +8,33 @@ export default function MESelect(props: DropDownPickerProps<any>) {
   return (
     <DropDownPicker
       { ...props }
+      language='ID'
+      listMode='MODAL'
       searchable
       theme="LIGHT"
+      disableBorderRadius={true}
+      style={{
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderRadius: 0,
+        borderTopColor: 'none',
+        paddingHorizontal: 0
+      }}
+      showTickIcon={false}
+      containerStyle={{
+        backgroundColor: 'white',
+        borderColor: Colors.light.blues.blue1,
+      }}
+      modalContentContainerStyle={{
+        margin: 24
+      }}
+      selectedItemContainerStyle={{
+        backgroundColor: Colors.light.blues.blue1,
+        borderRadius: 8
+      }}
+      selectedItemLabelStyle={{
+        color: 'white'
+      }}
     />
   )
 }
