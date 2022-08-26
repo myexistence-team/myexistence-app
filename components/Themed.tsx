5a94089b4995a0ebdc4e5a86d699c2ed178c59fe
FarizioKautsar <kheruzy@gmail.com>
2022-08-26 18:36:29 +0700

    View themed update

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -41,5 +41,5 @@ export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+  return <DefaultView style={[style]} {...otherProps} />;
 }
