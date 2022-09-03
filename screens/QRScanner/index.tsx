4905a227bf715970d0f1866b41d3497328ebd98e
FarizioKautsar <kheruzy@gmail.com>
2022-09-03 18:43:07 +0700

    Can scan class qr code

--- screens/QRScanner/index.tsx
+++ screens/QRScanner/index.tsx
@@ -1,16 +1,49 @@
-import { View, Text, StyleSheet, StatusBar, Platform } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, StatusBar, Platform, Alert } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
 import { RootStackScreenProps } from '../../navTypes'
 import MEContainer from '../../components/MEContainer';
 import MEHeader from '../../components/MEHeader';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import ScheduleCard from '../../components/ScheduleCard';
+import { Schedule } from '../../types';
+import { textStyles } from '../../constants/Styles';
+import MEButton from '../../components/MEButton';
+import { useNavigation } from '@react-navigation/native';
+import { createPresenceInSchedule } from '../../actions/scheduleActions';
+import { AuthContext, SchoolContext } from '../../contexts';
+import { FirebaseError } from 'firebase/app';
+import MESpinner from '../../components/MESpinner';
+
+export function QRScanSuccess({
+  schedule
+}: {
+  schedule: Schedule
+}) {
+  const navigation = useNavigation();
+  return (
+    <MEContainer>
+      <Text style={[textStyles.heading3, { textAlign: 'center', marginBottom: 64 }]}>Berhasil hadir!</Text>
+      <MEButton
+        onPress={() => {
+          navigation.navigate('Root', {
+            screen: 'Home'
+          })
+        }}
+      >
+        Kembali ke Home
+      </MEButton>
+    </MEContainer>
+  )
+}
 
 export default function QRScanner({ route }: RootStackScreenProps<'Scanner'>) {
   const { scheduleId, schedule } = route.params;
-
+  const {school} = useContext(SchoolContext);
+  const {auth} = useContext(AuthContext);
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [scanned, setScanned] = useState(false);
+  const [scanning, setScanning] = useState(false);
+  const [succeed, setSucceed] = useState(false);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -23,13 +56,38 @@ export default function QRScanner({ route }: RootStackScreenProps<'Scanner'>) {
 
   const handleBarCodeScanned = ({ type, data } : any) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    setScanning(true);
+    createPresenceInSchedule(
+      auth.uid,
+      school.id, 
+      schedule.classId, 
+      scheduleId, 
+      data
+    ).then(() => {
+        setScanning(false);
+        setSucceed(true);
+      })
+      .catch((e: FirebaseError) => {
+        console.log("ERROR", e.code)
+        Alert.alert(
+          'Oops!',
+          e.message,
+          [
+            { 
+              text: "OK", 
+              onPress: () => {
+                setScanned(false)
+              } 
+            }
+          ]
+        )
+      })
   };
 
   return (
     <View
       style={{
-        flex: 1
+        flex: succeed? 0 : 1,
       }}
     >
       <MEContainer>
@@ -45,33 +103,51 @@ export default function QRScanner({ route }: RootStackScreenProps<'Scanner'>) {
         }
       </MEContainer>
       {
-        hasPermission === true ? (
-          <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={[
-              {
-                flex: 1,
-                height: '100%',
-                position: 'absolute',
-                top: Platform.OS === 'android' ? 80 : 128,
-                left: 0,
-                bottom: 0,
-                right: 0
-              }
-            ]}
+        !succeed ? (
+          <>
+            {
+              scanning ? (
+                <View
+                  style={{
+                    flex: 1
+                  }}
+                >
+                  <MESpinner/>
+                </View>
+              ) : hasPermission === true ? (
+                <BarCodeScanner
+                  onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                  style={[
+                    {
+                      flex: 1,
+                      height: '100%',
+                      position: 'absolute',
+                      top: Platform.OS === 'android' ? 80 : 128,
+                      left: 0,
+                      bottom: 0,
+                      right: 0
+                    }
+                  ]}
+                />
+              ) : null
+            }
+            <View
+              style={{
+                padding: 24
+              }}
+            >
+              <ScheduleCard
+                schedule={schedule}
+                disableScanButton={true}
+              />
+            </View>
+          </>
+        ) : (
+          <QRScanSuccess
+            schedule={schedule}
           />
-        ) : null
+        )
       }
-      <View
-        style={{
-          padding: 24
-        }}
-      >
-        <ScheduleCard
-          schedule={schedule}
-          disableScanButton={true}
-        />
-      </View>
     </View>
   )
 }
