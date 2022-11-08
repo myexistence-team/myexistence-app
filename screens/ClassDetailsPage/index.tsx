6c2cd1f8fe0949871b99856d1bcf3b522967f053
FarizioKautsar <kheruzy@gmail.com>
2022-11-08 12:35:21 +0700

    Used UsersContext in Class Details

--- screens/ClassDetailsPage/index.tsx
+++ screens/ClassDetailsPage/index.tsx
@@ -6,22 +6,20 @@ import ClassScheduleCard from "../../components/ClassScheduleCard";
 import MEContainer from "../../components/MEContainer";
 import MEHeader from "../../components/MEHeader";
 import MESpinner from "../../components/MESpinner";
-import ScheduleCard from "../../components/ScheduleCard";
 import { textStyles } from "../../constants/Styles";
-import { ProfileContext } from "../../contexts";
+import { ProfileContext, UsersContext } from "../../contexts";
 import { firestore } from "../../firebase";
 import { ClassScreenProps } from "../../navTypes";
-import { Class, Profile } from "../../types";
+import { Class } from "../../types";
 
 export default function ClassDetailsPage({ route }: ClassScreenProps) {
   const { classId } = route.params;
   const navigation = useNavigation();
   
   const { profile } = useContext(ProfileContext);
+  const { users, setUsers } = useContext(UsersContext);
   const [classRoom, setClass] = useState<Class | any>(null);
   const [schedules, setSchedules] = useState<any[]>([]);
-  const [teachers, setTeachers] = useState<any[]>([]);
-  const [students, setStudents] = useState<any[]>([]);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -32,81 +30,65 @@ export default function ClassDetailsPage({ route }: ClassScreenProps) {
     orderBy('start'),
   )
 
-  const classQuery = query(collection(
-    firestore, 
-    `schools/${profile.schoolId}/classes`),
-    where(documentId(), '==', classId)
-  )
-
-  function loadData() {
+  async function loadData() {
     if (profile.classIds?.length) {  
       setIsLoading(true);     
       setClass(null);
 
-      getDoc((doc(
+      const classSnap = await getDoc((doc(
         firestore,
         'schools', 
         profile.schoolId,
         'classes', 
         classId
-      ))).then((classSnap) => {
-        if (classSnap.exists()) {          
-          const classroom = classSnap.data();
-          setClass(classroom);
-          setIsLoading(false);
+      )))
+      if (classSnap.exists()) {          
+        const classroom = classSnap.data();
+        setClass(classroom);
+        setIsLoading(false);
 
-          // Get Students
-          if (classroom.studentIds != 0) {            
-            getDocs(
+        // Get Students
+        if (classroom.studentIds.length != 0) {   
+          const notStoredIds: string[] = classroom.studentIds.filter((sId: string) => !Object.keys(users).includes(sId));
+          if (notStoredIds.length) {
+            const studentSnaps = await getDocs(
               query(collection(firestore,`users`),
-                where(documentId(), 'in', classroom.studentIds), limit(3)
-              )).then((docs) => {
-                const studentArr: any = [];
-                docs.forEach((doc) => {
-                  studentArr.push({ 
-                    ...doc.data(),
-                    id: doc.id
-                  });
-                })
-                setStudents(studentArr);
-            })
-          }
-
-          // Get Teacher
-          if (classroom.teacherIds != 0) {            
-            getDocs(
-              query(collection(firestore, `users`),
-                where(documentId(), 'in', classroom.teacherIds)
-              )).then((docs) => {
-                const teachArr: any = [];
-                docs.forEach((doc) => {
-                  teachArr.push({ 
-                    ...doc.data(),
-                    id: doc.id
-                  });
-                })
-              setTeachers(teachArr);
-            })
-          }
-
-          // Get Schedules
-          if (classroom) {
-            getDocs(scheduleQuery).then((docs) => {
-              const docsArr: any[] = [];
-              docs.forEach((doc) => {
-                docsArr.push({ 
-                  ...doc.data(),
-                  id: doc.id,
-                  className: classroom?.name,
-                  classDescription: classroom?.description,
-                });
-              })
-              setSchedules(docsArr);
-              setIsLoading(false);
-            })
+              where(documentId(), 'in', notStoredIds), limit(3)
+            ))
+            const userObjs = studentSnaps.docs.reduce((a, b) => ({ ...a, [b.id]: b.data() }), {});
+            setUsers((prevUsers: any) => ({ ...prevUsers, ...userObjs }));
           }
         }
-      })
+
+        // Get Teacher
+        if (classroom.teacherIds.length != 0) {      
+          const notStoredIds: string[] = classroom.teacherIds.filter((tId: string) => !Object.keys(users).includes(tId));
+          if (notStoredIds.length) {
+            const teacherSnaps = await getDocs(
+              query(collection(firestore, `users`),
+              where(documentId(), 'in', classroom.teacherIds)
+            ))
+            const userObjs = teacherSnaps.docs.reduce((a, b) => ({ ...a, [b.id]: b.data() }), {});
+            setUsers((prevUsers: any) => ({ ...prevUsers, ...userObjs }));
+          }
+        }
+
+        // Get Schedules
+        if (classroom) {
+          const scheduleSnaps = await getDocs(scheduleQuery)
+          const docsArr: any[] = [];
+          scheduleSnaps.forEach((doc) => {
+            docsArr.push({ 
+              ...doc.data(),
+              id: doc.id,
+              className: classroom?.name,
+              classDescription: classroom?.description,
+            });
+          })
+          setSchedules(docsArr);
+        }
+        setIsLoading(false);
+      }
       
     } else {
       setIsLoading(false);
@@ -144,26 +126,36 @@ export default function ClassDetailsPage({ route }: ClassScreenProps) {
               <Text style={[textStyles.body1, {marginBottom: 12}]}>{classRoom.description}</Text>
 
               <Text style={[textStyles.body2, {marginTop: 16, marginBottom: 4}]}>Pengajar</Text>
-                { teachers.length ? (
-                    teachers.map((person) => (
-                      <Text key={person.id} style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
-                        {person.displayName} 
-                      </Text>
-                    ))
-                  ) : <Text style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
-                        - 
-                      </Text>
+                { classRoom.teacherIds.length ? 
+                    classRoom.teacherIds.map((tId: string) => {
+                      const teacher = users[tId];
+                      return (
+                        <Text key={tId} style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
+                          {teacher?.displayName} 
+                        </Text>
+                      )
+                    }
+                  ) : (
+                    <Text style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
+                      - 
+                    </Text>
+                  )
                 }
 
               <Text style={[textStyles.body2, {marginTop: 16, marginBottom: 4}]}>Pelajar</Text>
-                { students.length ?
-                  (students.map((person) => (
-                    <Text key={person.id} style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
-                      {person.displayName} 
+                { classRoom.studentIds.length ?
+                  (classRoom.studentIds.map((sId: string) => {
+                    const student = users[sId]
+                    return (
+                      <Text key={sId} style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
+                        {student?.displayName} 
+                      </Text>
+                    )
+                  })) : (
+                    <Text style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
+                      - 
                     </Text>
-                  ))) : <Text style={[textStyles.body1, { fontFamily: 'manrope-bold' }]}>
-                          - 
-                        </Text>
+                  )
                 }
                 { 
                   classRoom.studentIds.length > 3 ?  (
@@ -180,8 +172,6 @@ export default function ClassDetailsPage({ route }: ClassScreenProps) {
                   ))
                 }
             </>              
-              
-            
           }
       </MEContainer>
   )
