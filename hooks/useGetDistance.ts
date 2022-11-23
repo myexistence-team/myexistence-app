dc43364d6cdb7ce53c60aa037a8048b3f072566f
FarizioKautsar <kheruzy@gmail.com>
2022-11-23 20:13:44 +0700

    useGetDistance hook

---
+++ hooks/useGetDistance.ts
@@ -0,0 +1,24 @@
+import { useContext, useEffect, useMemo } from "react"
+import { LocationContext } from "../contexts"
+import { getLocationDistance } from "../utils/utilFunctions";
+
+export default function useGetDistance(locationToCompare: {
+  latitude: number,
+  longitude: number
+}) {
+  const { location, startForegroundLocation } = useContext(LocationContext);
+  useEffect(() => {
+    startForegroundLocation();
+  }, [])
+  const distance: number = useMemo(() => {
+    if (locationToCompare && location) {
+      return getLocationDistance(location, { 
+        latitude: locationToCompare.latitude,
+        longitude: locationToCompare.longitude,
+      })
+    } else {
+      return 999;
+    }
+  }, [locationToCompare, location]);
+  return distance;
+}
